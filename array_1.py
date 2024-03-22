290d0bacb5af959903fe6a63752cc52d95dae620
Casey Jones <caseysemail52@gmail.com>
2024-03-21 18:21:41 -0600

    Create array_1.py

---
+++ array_1.py
@@ -0,0 +1,36 @@
+#problem 1
+print('Casey Jones')
+#problem 2
+cars = ['Ford','Chrysley','Dodge','Ram','Jeep','Chevy','GMC']
+#problem 3
+print(cars)
+#problem 4
+print(len(cars))
+#problem 5
+cars.append('Buick')
+#problem 6
+print(cars)
+#problem 7
+print(cars[3])
+#problem 8
+cars.insert(2,'Toyota')
+#problem 9
+print(cars)
+#problem 10
+cars.pop(4)
+#problem 11
+print(cars)
+#problem 12
+cars.sort()
+#problem 13
+print(cars)
+#problem 14
+cars.sort(reverse= True)
+#problem 15
+print(cars)
+#problem 16
+my_array_length = len(cars)
+#problem 17
+array_string = 'The length of my array is '
+#problem 18
+print(array_string + str(my_array_length))
